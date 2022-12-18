4acd28955960e736e185f7de603a5d3070c7ba62
Volle <github_volle@vllmr.eu>
2022-12-18 18:22:15 +0100

    Fix updater script

--- files/51-aurora_playstore_aa.sh
+++ files/51-aurora_playstore_aa.sh
@@ -8,15 +8,15 @@
 
 list_files() {
 cat <<EOF
-system/priv-app/AuroraServices.apk
-system/etc/permissions/com.aurora.services.xml
-system/priv-app/Phonesky/Phonesky.apk
-system/etc/permissions/permissions-com.android.vending.xml
-system/priv-app/AndroidAuto/AndroidAutoStubPrebuilt.apk
-system/etc/permissions/privapp-permissions-com.google.android.projection.gearhead.xml
-system/priv-app/gappstub/gappsstub.apk
-system/priv-app/speechservicestub/speech-services-stub.apk
-system/addon.d/51-aurora_playstore_aa.sh
+priv-app/AuroraServices.apk
+etc/permissions/com.aurora.services.xml
+priv-app/Phonesky/Phonesky.apk
+etc/permissions/permissions-com.android.vending.xml
+priv-app/AndroidAuto/AndroidAutoStubPrebuilt.apk
+etc/permissions/privapp-permissions-com.google.android.projection.gearhead.xml
+priv-app/gappstub/gappsstub.apk
+priv-app/speechservicestub/speech-services-stub.apk
+addon.d/51-aurora_playstore_aa.sh
 EOF
 }
 
@@ -49,5 +49,7 @@ case "$1" in
       chmod 644 $f
       chmod 755 $(dirname $f)
     done
+    rm -rf system/priv-app/FakeStore
+    rm -rf priv-app/FakeStore
   ;;
 esac
